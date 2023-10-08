9b71e8471aebc49b5e89e765d87a55071cb1eebf
mygit-sumit <134057978+mygit-sumit@users.noreply.github.com>
2023-10-09 00:51:31 +0530

    2nd commit

--- src/com/first/project/Hello.java
+++ src/com/first/project/Hello.java
@@ -4,6 +4,12 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello to Java World");
+		int a=9;
+		int b=9;
+		
+		int c= a+b;
+		System.out.println("c: "+c);
+				
 	}
 
 }
